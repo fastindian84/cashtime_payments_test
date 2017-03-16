3b1cacda209b4dde5dffd487bd096d7f0f30f9b9
Yaroslav Senishyn <fastindian84@gmail.com>
2017-03-16 16:26:27 +0200

    Find by order uid

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,7 @@ class CallbacksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    req = AppRequest.find(params[:order_uid])
+    req = AppRequest.find_by!(order_uid: params[:order_uid])
     req.status = params[:status]
     req.response = params[:callback].permit!.to_h
     req.save
